84d2908534cc6dbb5440944c067309efa0b9665f
Konrad <vobis08gw@gmail.com>
2024-06-27 19:55:14 +0200

    styled copy button

--- source/typerio-xyz/components/InstallLink.tsx
+++ source/typerio-xyz/components/InstallLink.tsx
@@ -1,3 +1,4 @@
+"use client";
 import "@/styles/InstallLink.scss";
 interface ComponentProps {
   name: string;
@@ -5,10 +6,25 @@ interface ComponentProps {
 }
 
 const InstallLink: React.FC<ComponentProps> = ({ name, command }) => {
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(command);
+    } catch (error) {
+      console.error(`Cannot copy: ${error}`);
+    }
+  };
   return (
     <div className="InstallLinkComponent">
-      <h2>{name}</h2>
-      <p>{command}</p>
+      <h2 className="title">{name}</h2>
+      <p className="wrapper">
+        <span className="command">{command}</span>
+        <img
+          className="icon"
+          src="/copy.svg"
+          alt="copy icon"
+          onClick={handleCopy}
+        />
+      </p>
     </div>
   );
 };

279818ff24a0721276c271840c8053194a2bcfe7
Konrad <vobis08gw@gmail.com>
2024-07-03 13:12:53 +0200

    mdx config

--- source/typerio-xyz/next.config.mjs
+++ source/typerio-xyz/next.config.mjs
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 import path from "path";
+import withMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import remarkGithubBlockquoteAlert from "remark-github-blockquote-alert";
 
-export default {
+const mdxOptions = {
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm, remarkGithubBlockquoteAlert],
+    rehypePlugins: [],
+    // any other MDX options can go here
+  },
+};
+
+export default withMDX(mdxOptions)({
   sassOptions: {
     includePaths: [path.join(new URL("./styles", import.meta.url).pathname)],
   },
   reactStrictMode: false,
-};
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});

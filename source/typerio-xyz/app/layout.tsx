1271e90ee4b0aeaeccf7a7190b83ecd95c165408
Konrad <vobis08gw@gmail.com>
2024-07-05 21:50:48 +0200

    added favicon & fixed firefox problem

--- source/typerio-xyz/app/layout.tsx
+++ source/typerio-xyz/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
         {children}
         <Footer />

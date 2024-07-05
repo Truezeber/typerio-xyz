9404c271af9deb8cbc933ad8ab7b65866df63502
Konrad <vobis08gw@gmail.com>
2024-07-05 12:50:54 +0200

    footer styling

--- source/typerio-xyz/components/mdx-layout.tsx
+++ source/typerio-xyz/components/mdx-layout.tsx
@@ -1,14 +1,17 @@
 import "@/styles/_global.scss";
 import "@/styles/docs.scss";
 import "@/styles/atom-one-dark.css";
-import Navbar from "@/components/DocsNavbar";
 import "remark-github-blockquote-alert/alert.css";
+
+import Navbar from "@/components/DocsNavbar";
+import Footer from "@/components/Footer";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       <div className="page">{children}</div>
+      <Footer />
     </>
   );
 }

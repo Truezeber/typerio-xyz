153ab161ab2ba432301f7969cf63a13137c9089e
Konrad <vobis08gw@gmail.com>
2024-08-03 21:20:19 +0200

    recreated file structure

---
+++ source/typerio-xyz/app/docs/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+
+import "@/styles/_global.scss";
+
+export const metadata: Metadata = {
+  title: "Typerio",
+  description: "Typerio website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/typerio-logo.svg" sizes="any" />
+      </head>
+      <body>
+        {children}
+        {/* <Footer /> */}
+      </body>
+    </html>
+  );
+}

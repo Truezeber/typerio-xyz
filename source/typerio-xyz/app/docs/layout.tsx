f43dd7a456390d1e08b7587ea9fb69eeb9339d85
Konrad <vobis08gw@gmail.com>
2024-08-04 17:49:08 +0200

    added mdx code styling

--- source/typerio-xyz/app/docs/layout.tsx
+++ source/typerio-xyz/app/docs/layout.tsx
@@ -3,6 +3,10 @@ import type { Metadata } from "next";
 import "@/components/DocsNavbar";
 
 import "@/styles/_global.scss";
+import "@/styles/docs.scss";
+import "@/styles/edge-light.css";
+import "remark-github-blockquote-alert/alert.css";
+
 import Navbar from "@/components/DocsNavbar";
 
 export const metadata: Metadata = {

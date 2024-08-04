979648f15dfab02dd7d0eb3de42047bf18868b77
Konrad <vobis08gw@gmail.com>
2024-08-04 16:05:07 +0200

    basic navbar structure

--- source/typerio-xyz/components/DocsNavbar.tsx
+++ source/typerio-xyz/components/DocsNavbar.tsx
@@ -1,35 +1,165 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import "@/styles/docsNavbar.scss";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    return pathname === path ? "active" : "";
+  };
+
   return (
     <div className="navbar">
-      <div className="navbar-wrapper">
-        <div className="navbar-top">
-          <p className="navbar-title">
-            <a href="/docs">Typerio Docs</a>
-          </p>
-
-          <div className="navbar-maingroup">
-            <p className="navbar-maingroup-title">Getting started</p>
-            <a href="/docs/typerio" className="navbar-link">
-              typerio
-            </a>
-            <a href="/docs/typerio-react" className="navbar-link">
-              typerio-react
-            </a>
-          </div>
-          <div className="navbar-maingroup">
-            <p className="navbar-maingroup-title">Other</p>
-            <a href="/docs/contributing" className="navbar-link">
-              Contributing
-            </a>
-          </div>
-        </div>
-        <div className="navbar-bottom">
-          <p className="navbar-title">
-            <a href="https://typerio.xyz/">Homepage</a>
-          </p>
-        </div>
+      <div className="navTop">
+        <h1 className="title">
+          <Link
+            className={`link ${pathname === "/docs" ? "active" : ""}`}
+            href="/docs"
+          >
+            Typerio Docs
+          </Link>
+        </h1>
+      </div>
+      <div className="section">
+        <h2 className="title">Typerio</h2>
+        <ul>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/typerio-html/instalation" ? "active" : ""
+              }`}
+              href="/docs/typerio-html/instalation"
+            >
+              Instalation
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/typerio-html/basic-usage" ? "active" : ""
+              }`}
+              href="/docs/typerio-html/basic-usage"
+            >
+              Basic usage
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/typerio-html/advanced-usage" ? "active" : ""
+              }`}
+              href="/docs/typerio-html/advanced-usage"
+            >
+              Advanced usage
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/typerio-html/examples" ? "active" : ""
+              }`}
+              href="/docs/typerio-html/examples"
+            >
+              Examples
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="section">
+        <h2 className="title">Typerio Reacy</h2>
+        <ul>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/typerio-react/instalation" ? "active" : ""
+              }`}
+              href="/docs/typerio-react/instalation"
+            >
+              Instalation
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/typerio/usage" ? "active" : ""
+              }`}
+              href="/docs/typerio/usage"
+            >
+              Component usage
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/typerio/typescript" ? "active" : ""
+              }`}
+              href="/docs/typerio/typescript"
+            >
+              Typescript
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/typerio/examples" ? "active" : ""
+              }`}
+              href="/docs/typerio/examples"
+            >
+              Examples
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="section">
+        <h2 className="title">Contributing</h2>
+        <ul>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/contributing/coc" ? "active" : ""
+              }`}
+              href="/docs/contributing/coc"
+            >
+              Code of conduct
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/contributing/chosing-project"
+                  ? "active"
+                  : ""
+              }`}
+              href="/docs/contributing/chosing-project"
+            >
+              Chosing a project
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/contributing/issues" ? "active" : ""
+              }`}
+              href="/docs/contributing/issues"
+            >
+              Reporting issues
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`link ${
+                pathname === "/docs/contributing/pull request" ? "active" : ""
+              }`}
+              href="/docs/contributing/pull request"
+            >
+              Pull requests
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );

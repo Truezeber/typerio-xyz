7ff643649beb0fb18ee6be50bbf58251c6350146
Konrad <vobis08gw@gmail.com>
2024-08-05 04:26:47 +0200

    importing font in head

--- source/typerio-xyz/app/layout.tsx
+++ source/typerio-xyz/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/typerio-logo.svg" sizes="any" />
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');
+        </style>
       </head>
       <body>
         <div className="body">{children}</div>
